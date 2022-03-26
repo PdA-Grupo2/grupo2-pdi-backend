5e7a64cc2dc676c674f757c607266945a37ed821
RealDantass <101597332+RealDantass@users.noreply.github.com>
2022-03-26 11:07:43 -0300

    Add files via upload

---
+++ model/dados.js
@@ -0,0 +1,7 @@
+exports.produtos = [
+    {id:1, nome: 'teclado', valor: 50.00, quantidade: 6},
+    {id:2, nome: 'capinha', valor: 25.00, quantidade: 20},
+    {id:3, nome: 'pelicula', valor: 10.00, quantidade: 25},
+    {id:4, nome: 'fone', valor: 20.00, quantidade: 18},
+    {id:5, nome: 'strep fone', valor: 15.00, quantidade: 15}
+]
